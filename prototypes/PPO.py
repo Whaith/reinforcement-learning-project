4502a1799090f3a3ea7255df3bbf05ea105a8c7a
Maxim <maxim.afteniy@gmail.com>
2019-12-01 22:57:57 +0200

    Wrapped training into a Class, PPO_Agent in prototypes/PPO.py

--- prototypes/PPO.py
+++ prototypes/PPO.py
@@ -1,4 +1,3 @@
-#%%
 import sys
 sys.path.append('.')
 
@@ -15,11 +14,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.distributions import Categorical
-from tensorboardX import SummaryWriter
+# from utils import RAdam
 
-from utils import hard_update
+def moving_average(x, N):
+    return np.convolve(x, np.ones(N, ), mode='valid') / N
 
-#%%
 class Policy(nn.Module):
     def __init__(self):
         super(Policy, self).__init__()
@@ -27,145 +26,167 @@ class Policy(nn.Module):
         self.action_head = nn.Linear(128, 2)
         self.value_head = nn.Linear(128, 1)
 
-        self.saved_actions = []
+        self.actions = []
+        self.logprobs = []
         self.rewards = []
+        self.dones = []
+        self.states = []
 
-    def forward(self, x, only_value=False):
-        if only_value:
-            with torch.no_grad():
-                x = F.relu(self.affine1(x))
-                state_values = self.value_head(x)
-                return state_values
-
+    def forward(self, x):
         x = F.relu(self.affine1(x))
         action_scores = self.action_head(x)
         state_values = self.value_head(x)
-        return F.softmax(action_scores, dim=-1), state_values
+        return F.softmax(action_scores, dim=-1), state_values.squeeze()
 
-
-SavedAction = namedtuple('SavedAction', ['log_prob', 'value', 'ratios', 'entropy'])
-
-# define policies
 policy = Policy()
 policy_old = Policy()
-hard_update(policy_old, policy)
+policy_old.load_state_dict(policy.state_dict())
 
 optimizer = optim.RMSprop(policy.parameters(), lr=3e-3)
+# optimizer = RAdam(policy.parameters(), lr=3e-3)
 eps = np.finfo(np.float32).eps.item()
 
-def select_action(state, old_policy):
-    state = torch.from_numpy(state).float()
-    probs, state_value = policy(state)
-    with torch.no_grad():
-        probs_old, _ = policy_old(state)
-    m = Categorical(probs)
-    action = m.sample()
-    r_t = probs[action]/probs_old[action]
-    policy.saved_actions.append(SavedAction(m.log_prob(action), state_value, r_t, m.entropy()))
-    return action.item()
+# def reset_globals():
+#     global policy
+#     global policy_old
+#     global optimizer
+    
+    # policy = Policy()
+    # policy_old = Policy()
+    # policy_old.load_state_dict(policy.state_dict())
+    
+    # optimizer = optim.RMSprop(policy.parameters(), lr=3e-3)
 
-"GIVEN rewards array from rollout return the returns with zero mean and unit std"        
-def discount_rewards(rewards_arr, gamma, start_value=0):
-    R = start_value
-    returns = []
-    for r in rewards_arr[::-1]:
-        R = r + R*gamma
-        returns.insert(0, R)
-    returns = torch.tensor(returns)
-    if len(returns) == 1:
-        return returns
-    return (returns - returns.mean())/(returns.std() + eps)
+class PPO_Agent():
 
-def compute_single_actor_loss_element(r_t, advantage_t, clip_value=0.2):
-    first_val = r_t * advantage_t
-    second_val = torch.clamp(r_t, 1-clip_value, 1+clip_value) * advantage_t
-    if first_val.item() > second_val.item():
-        return second_val 
-    else:
-        return first_val
+    def __init__(self):
+        self.gamma = 0.99
+        self.batch_update_freq = 300
+        self.clip_val = 0.2
+        self.c2 = 0.0001
+        self.n_epochs = 4
 
-def train_on_rollout(gamma, writer, T):
-    returns = discount_rewards(policy.rewards, gamma)
-    actor_loss = []
-    critic_loss = []
-    entropy_objective = []
-    for (log_prob, value, ratio, entropy), r in zip(policy.saved_actions, returns):
-        advantage = r - value.item()
-        # PPO changes the actor loss
-        actor_loss.append(-compute_single_actor_loss_element(ratio, advantage))
-        entropy_objective.append(-entropy)
-        critic_loss.append(F.smooth_l1_loss(value, torch.tensor([r])))
-    optimizer.zero_grad()
-    loss = torch.stack(actor_loss).mean() + torch.stack(critic_loss).mean()
-    loss += 0.1 * torch.stack(entropy_objective).mean()
-    loss.backward()
-    writer.add_scalar("actor and critic loss", loss.item(), T)
-    optimizer.step()
-    del policy.rewards[:]
-    del policy.saved_actions[:]
+        self.policy = Policy()
+        self.policy_old = Policy()
+        self.policy_old.load_state_dict(self.policy.state_dict())
+    
+        self.optimizer = optim.RMSprop(self.policy.parameters(), lr=3e-3)
+        
+    def select_action(self, state):
+        state = torch.from_numpy(state).float()
+        probs, state_value = self.policy_old(state)
+        m = Categorical(probs)
+        action = m.sample()
+        self.policy.actions.append(action.item())
+        self.policy.logprobs.append(m.log_prob(action).item())
+        self.policy.states.append(state)
+    #     policy.saved_actions.append(SavedAction(m.log_prob(action), state_value))
+        return action.item()
 
-def train_on_batch(observation, done, writer, gamma, T):
-    returns = None
-    if done:
-        returns = discount_rewards(policy.rewards, gamma, start_value=0)
-    else:
-        next_state = torch.from_numpy(observation).float()
-        final_value = policy(next_state, only_value=True)
-        returns = discount_rewards(policy.rewards, gamma, start_value=final_value.item())
-    actor_loss = []
-    critic_loss = []
-    for (log_prob, value), r in zip(policy.saved_actions, returns):
-        advantage = r - value.item()
-        actor_loss.append(-log_prob * advantage)
-        critic_loss.append(F.mse_loss(value, torch.tensor([r])))
-    optimizer.zero_grad()
-    loss_actor = torch.stack(actor_loss).mean()
-    writer.add_scalar("actor loss", loss_actor, T)
-    loss_critic = torch.stack(critic_loss).mean()
-    writer.add_scalar("critic loss", loss_actor, T)
-    loss = loss_actor + loss_critic
-    loss.backward()
-    optimizer.step()
-    del policy.rewards[:]
-    del policy.saved_actions[:]
+    "GIVEN rewards array from rollout return the returns with zero mean and unit std"        
+    def discount_rewards(self, rewards_arr, dones, gamma, final_value=0):
+        R = final_value
+        returns = []
+        zipped = list(zip(rewards_arr, dones))
+        for (r, done) in zipped[::-1]:
+            if done:
+                R = 0
+            R = r + R*gamma
+            returns.insert(0, R)
+        returns = torch.tensor(returns)
+        return (returns - returns.mean())/(returns.std() + eps)
 
-def learn_episodic_A2C(N_eps, max_ep_steps, writer):
-    df = 0.99
+    def batch_update_agent(self, final_obs=None, done=True):
+        state = torch.from_numpy(final_obs).float()
+        _, state_value = self.policy(state)
+        final_value = state_value.detach() if not done else 0.0
+        returns = self.discount_rewards(self.policy.rewards, \
+            self.policy.dones, self.gamma, final_value)
+        
+        states = torch.stack(self.policy.states).float()
+    #     print(states.shape)
+        old_actions = self.policy.actions
+    #     print(old_actions)
+        old_logprobs = torch.tensor(self.policy.logprobs).float()
+    #     print(old_logprobs.shape)
+        
+        # PPO OLD VALUES
+        for i in range(self.n_epochs):
+            # Calculate needed values    
+            p, v = self.policy.forward(states)
+            m = Categorical(p)
+            c = m.log_prob(torch.tensor(old_actions))
+            entr = m.entropy()
+
+            # value fn loss
+            loss_vf = F.mse_loss(v, returns)
+
+            # surrogate loss
+            advantage = returns - v.detach()
+            r_ts = torch.exp(c - old_logprobs)
+            loss_surr = - (torch.min(r_ts * advantage, \
+                torch.clamp(r_ts, 1-self.clip_val, 1+self.clip_val) * advantage)).mean()
+            
+            # maximize entropy bonus
+            loss_entropy = - self.c2 * entr.mean()
+
+            # the total_loss
+            loss_total = loss_vf + loss_surr + loss_entropy
+            
+            # step
+            self.optimizer.zero_grad()
+            loss_total.backward()
+            self.optimizer.step()
+
+        self.policy_old.load_state_dict(self.policy.state_dict())    
+        
+        del self.policy.actions[:]
+        del self.policy.states[:]
+        del self.policy.logprobs[:]
+        del self.policy.rewards[:]
+        del self.policy.dones[:]
+
+def learn_PPO_single_threaded(N_eps=500, max_ep_steps=500):
     rewards = []
     env = gym.make('CartPole-v0')
-    batch_update = 20
     env._max_episode_steps = max_ep_steps
     T = 0
-    initially_updated = False
+
+    agent = PPO_Agent()
+    
     for i_episode in range(N_eps):
         observation = env.reset()
         total_r = 0
         for t in range(100000):
-            action = select_action(observation, policy_old)
-
+            T += 1
+            action = agent.select_action(observation)
             observation, reward, done, info = env.step(action)
-            policy.rewards.append(reward)
+
+            agent.policy.rewards.append(reward)
+            agent.policy.dones.append(done)
+
             total_r += reward
-            # if done or ((t % batch_update) == 0):
-                # train_on_batch(observation, done, writer, df, T)
+            if T % agent.batch_update_freq == 0:
+                agent.batch_update_agent(observation, done=True)
             if done:
-                train_on_rollout(df, writer, T)
-                if initially_updated:
-                    hard_update(policy_old, policy)
-                initially_updated = True
+#                 train_on_batch(0.99, observation, done)
                 if (i_episode + 1) % 100 == 0:                
                     print("Episode {} finished after {} timesteps".format(i_episode, t+1))
                 break
-            T += 1
-        writer.add_scalar("Ep reward", total_r, i_episode)
-
-    # writer.export_scalars_to_json("./all_scalars.json")
-    writer.close()
+        rewards.append(total_r)
     env.close()
     return rewards
-# N_EPS = 2000
-# rewards_A2C = learn_episodic_A2C(N_EPS, 500)
 
-if __name__ == '__main__':
-    writer = SummaryWriter()
-    learn_episodic_A2C(500, 500, writer)
+N_EPS = 1500
+
+rewards_PPO = learn_PPO_single_threaded(N_EPS, 500)
+plt.plot(moving_average(rewards_PPO, 150), label='RMSprop')
+
+# Try out radam
+# reset_globals()
+
+# # optimizer = RAdam(policy.parameters(), lr=3e-3)
+# rewards_PPO_RAdam = learn_PPO_single_threaded(N_EPS, 500)
+# plt.plot(moving_average(rewards_PPO_RAdam, 150), label='RAdam')
+# plt.legend()
+# plt.show()
